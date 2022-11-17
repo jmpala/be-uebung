45ec311d8b40e0ec341baac8e5ba5309f33b1067
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2022-11-17 15:14:15 +0100

    Sample HTML change to test POST redirection

--- src/Webapp/Controllers/SampleProcessController.php
+++ src/Webapp/Controllers/SampleProcessController.php
@@ -4,12 +4,13 @@ namespace Webapp\Controllers;
 
 use Framework\Contracts\Controller;
 use Framework\Http\Request;
+use Framework\Http\Response;
 
 class SampleProcessController implements Controller
 {
 
-    public function handle(Request $request)
+    public function handle(Request $request): Response
     {
-        return "Hello World Processing!";
+        return redirect('/');
     }
 }
