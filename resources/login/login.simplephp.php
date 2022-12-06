b471869a6ca3d34bef5e6878879606d1a03fa5b0
jmpala <juanmartinpalazzolo@icloud.com>
2022-12-06 15:43:13 +0100

    added names to the form inputs, in order to have those items on the $_post global

--- resources/login/login.simplephp.php
+++ resources/login/login.simplephp.php
@@ -13,10 +13,10 @@
     <div class="container-md">
         <div class="row vh-100 justify-content-center align-content-center">
             <h1 class="text-center">LBX Desk Sharing</h1>
-            <form class="loginform__form py-3 px-1 p-md-3 rounded-3">
-                <input type="text" id="email" class="loginform__username rounded-2 w-100" required>
+            <form class="loginform__form py-3 px-1 p-md-3 rounded-3" action="/login" method="post">
+                <input type="text" id="email" name="email" class="loginform__username rounded-2 w-100" required>
                 <label for="email" class="loginform__usernameLbl w-100">User Name</label>
-                <input type="password" id="password" class="loginform__password rounded-2 w-100" required>
+                <input type="password" id="password" name="password" class="loginform__password rounded-2 w-100" required>
                 <label for="password" class="loginform__passwordLbl w-100">Password</label>
                 <button id="submit-btn" type="submit" class="btn loginform__btn">Submit</button>
             </form>
