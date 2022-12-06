1267a4ff8f1d1e867751f2846827a8679021aa94
jmpala <juanmartinpalazzolo@icloud.com>
2022-12-06 15:38:47 +0100

    Basic Migrations scripts

---
+++ migrations/1-create-db.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS `desksharing_dev`;
+
+CREATE DATABASE IF NOT EXISTS `desksharing_dev`;
+
---
+++ migrations/2-create-users-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `desksharing_dev`.`users` (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE = InnoDB;
