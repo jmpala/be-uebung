588aa7029fb163bcd893675e4911dc3adceb892f
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2022-12-12 17:05:41 +0100

    added favicon to html

--- resources/overview/overview.simplephp.php
+++ resources/overview/overview.simplephp.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="/dist/img/favicon.png">
     <title>Overview Page</title>
 <script defer src="/dist/js/overview.5fa5d8d69c48333a6195.js"></script></head>
 <body class="d-flex flex-column min-vh-100">
