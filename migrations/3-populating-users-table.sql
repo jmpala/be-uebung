7d3634bfbfbf972ee5877fc96ac46192d6d7346e
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2022-12-12 11:49:19 +0100

    migrations minor correction - emails are now unique

--- migrations/3-populating-users-table.sql
+++ migrations/3-populating-users-table.sql
@@ -1,4 +1,4 @@
 INSERT INTO `desksharing_dev`.`users` (name, email, password)
 VALUES ('Martin Garrix', 'test1@test.com', 'Test@123'),
-('Skrillex', 'test1@test.com', 'Test@123'),
-('Carl Cox', 'test1@test.com', 'Test@123');
+('Skrillex', 'test2@test.com', 'Test@123'),
+('Carl Cox', 'test3@test.com', 'Test@123');
