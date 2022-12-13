af18430c807b801101ca7e1489c2915eb3de23bd
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2022-12-13 09:46:59 +0100

    Access web resources only when logged in

--- src/Framework/Middleware/Authentication/AuthenticationMidd.php
+++ src/Framework/Middleware/Authentication/AuthenticationMidd.php
@@ -13,15 +13,31 @@ class AuthenticationMidd extends AbstractHandler
     private int $secondsBeforeIdRegen = 300;
     private int $secondsBeforeSessionExpire = 360;
 
+    private array $publicRoutes = [
+        '/login'
+    ];
+
     protected function process(Request $request): Request
     {
         $sessionManager = container(SessionManager::class);
 
+        $this->checkIfLoggedIn($sessionManager, $request);
         $this->checkTimestampOrCreate($sessionManager);
         $this->checkSessionExpire($sessionManager);
         $this->regenerateSessionId($sessionManager);
 
         return $request;
+    }
+
+    private function checkIfLoggedIn(SessionManager $sessionManager, Request $request): void
+    {
+        if (in_array($request->uri(), $this->publicRoutes, false)) {
+            return;
+        }
+
+        if (!$sessionManager->isLoggedIn()) {
+            redirect('/login');
+        }
     }
 
     private function checkTimestampOrCreate(SessionManager $sessionManager): void
