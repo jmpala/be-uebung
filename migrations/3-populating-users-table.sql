94c2b6b7a4a919a8ae4b9a759d9c47c01e60546c
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2022-12-13 14:45:48 +0100

    added a user

--- migrations/3-populating-users-table.sql
+++ migrations/3-populating-users-table.sql
@@ -1,4 +1,5 @@
 INSERT INTO `desksharing_dev`.`users` (name, email, password)
 VALUES ('Martin Garrix', 'test1@test.com', '$2y$10$xRZ52HFAI5e.K8exVjYM7OVZ8adPGT7/Gb9.UU9jos0q1JxIendAC'),
 ('Skrillex', 'test2@test.com', '$2y$10$isNQDu60Y3sk6w2Djl6qCeVHgd35BQq4IE.dnlujrGuivCk1O1hKe'),
-('Carl Cox', 'test3@test.com', '$2y$10$RO1nQhjcXnWaSVWy1SjwN.6xTbOIHedB5Gzp6zeoGHvPQRAHjAVKW');
+('Carl Cox', 'test3@test.com', '$2y$10$RO1nQhjcXnWaSVWy1SjwN.6xTbOIHedB5Gzp6zeoGHvPQRAHjAVKW'),
+('Hernan Cattaneo', 'test4@test.com', '$2y$10$RO1nQhjcXnWaSVWy1SjwN.6xTbOIHedB5Gzp6zeoGHvPQRAHjAVKW');
