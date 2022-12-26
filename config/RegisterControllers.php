42bff1ec950df4b1b3556c9bbb8a9c608b56d889
Juan M Palazzolo <juanmartinpalazzolo@icloud.com>
2022-12-26 07:21:42 +0100

    Added ProcessLoginRESTController to the router

--- config/RegisterControllers.php
+++ config/RegisterControllers.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    // Webapp
     'GET:/' => \Webapp\Controllers\SampleController::class,
     'POST:/' => \Webapp\Controllers\SampleProcessController::class,
     'GET:/login' => \Webapp\Controllers\Login\ShowLoginPageController::class,
@@ -9,5 +10,7 @@ return [
     'GET:/overview' => \Webapp\Controllers\Overview\ShowOverviewPageController::class,
     'GET:/overview/{?page}' => \Webapp\Controllers\Overview\ShowOverviewPageController::class,
     'staticFile:/{file}' => \Webapp\Controllers\StaticFilesController::class,
-    'GET:/api/desks' => \Webapp\Controllers\SampleRestGETController::class,
+
+    // REST API
+    'POST:/api/login' => \RESTapi\Controllers\Login\ProcessLoginRESTController::class,
 ];
