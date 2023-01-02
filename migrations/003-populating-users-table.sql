541423197b388240ffe3fb8361c990ae2411bbea
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-01-02 11:53:16 +0100

    Change users default passwords

--- migrations/003-populating-users-table.sql
+++ migrations/003-populating-users-table.sql
@@ -1,8 +1,8 @@
 INSERT INTO `desksharing_dev`.`users` (name, email, password, role_id)
-VALUES ('User One', 'user1@user.com', '$2y$10$xRZ52HFAI5e.K8exVjYM7OVZ8adPGT7/Gb9.UU9jos0q1JxIendAC', 1),
-('User Two', 'user2@user.com', '$2y$10$isNQDu60Y3sk6w2Djl6qCeVHgd35BQq4IE.dnlujrGuivCk1O1hKe', 1),
-('User Three', 'user3@user.com', '$2y$10$RO1nQhjcXnWaSVWy1SjwN.6xTbOIHedB5Gzp6zeoGHvPQRAHjAVKW', 1),
-('User Four', 'user4@user.com', '$2y$10$RO1nQhjcXnWaSVWy1SjwN.6xTbOIHedB5Gzp6zeoGHvPQRAHjAVKW', 1),
-('TL One', 'tl1@user.com', '$2y$10$RO1nQhjcXnWaSVWy1SjwN.6xTbOIHedB5Gzp6zeoGHvPQRAHjAVKW', 2),
-('TL Two', 'tl2@user.com', '$2y$10$RO1nQhjcXnWaSVWy1SjwN.6xTbOIHedB5Gzp6zeoGHvPQRAHjAVKW', 2),
-('Admin Admin', 'admin@user.com', '$2y$10$RO1nQhjcXnWaSVWy1SjwN.6xTbOIHedB5Gzp6zeoGHvPQRAHjAVKW', 3);
+VALUES ('User One', 'user1@user.com', '$2y$10$BQ.bP5VhoSS/ENUlDnYTA.mWJK/K7lzD1YgYDKoxN8zX7dbCHOdw2', 1),
+('User Two', 'user2@user.com', '$2y$10$BQ.bP5VhoSS/ENUlDnYTA.mWJK/K7lzD1YgYDKoxN8zX7dbCHOdw2', 1),
+('User Three', 'user3@user.com', '$2y$10$BQ.bP5VhoSS/ENUlDnYTA.mWJK/K7lzD1YgYDKoxN8zX7dbCHOdw2', 1),
+('User Four', 'user4@user.com', '$2y$10$BQ.bP5VhoSS/ENUlDnYTA.mWJK/K7lzD1YgYDKoxN8zX7dbCHOdw2', 1),
+('TL One', 'tl1@user.com', '$2y$10$BQ.bP5VhoSS/ENUlDnYTA.mWJK/K7lzD1YgYDKoxN8zX7dbCHOdw2', 2),
+('TL Two', 'tl2@user.com', '$2y$10$BQ.bP5VhoSS/ENUlDnYTA.mWJK/K7lzD1YgYDKoxN8zX7dbCHOdw2', 2),
+('Admin Admin', 'admin@user.com', '$2y$10$BQ.bP5VhoSS/ENUlDnYTA.mWJK/K7lzD1YgYDKoxN8zX7dbCHOdw2', 3);
