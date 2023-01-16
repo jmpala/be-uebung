1a6afdfe1c6082a3bb39c312f947858c7ada2612
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-01-16 15:23:02 +0100

    update sample process controller, to response implementation

--- src/Webapp/Controllers/SampleProcessController.php
+++ src/Webapp/Controllers/SampleProcessController.php
@@ -5,12 +5,20 @@ namespace Webapp\Controllers;
 use Framework\Contracts\Controller;
 use Framework\Http\Request;
 use Framework\Http\Response;
+use Framework\Http\StatusCode;
 
 class SampleProcessController implements Controller
 {
+    private Response $response;
+
+    public function __construct()
+    {
+        $this->response = container(Response::class);
+    }
 
     public function handle(Request $request): Response
     {
+        $this->response->statusCode(StatusCode::OK);
         return redirect('/');
     }
 }
