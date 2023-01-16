8e6f217b88ec2697c7acc1b2eccbf5131f527a75
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-01-16 13:09:55 +0100

    Implementing service on controller

--- src/Webapp/Controllers/Overview/ShowOverviewPageController.php
+++ src/Webapp/Controllers/Overview/ShowOverviewPageController.php
@@ -3,7 +3,6 @@
 namespace Webapp\Controllers\Overview;
 
 use Framework\Contracts\Controller;
-use Framework\DAOs\BookingDAO;
 use Framework\Http\Request;
 use Framework\Http\Response;
 use Framework\Services\BookingService;
@@ -30,11 +29,14 @@ class ShowOverviewPageController implements Controller
             $currentPage = 1;
         }
 
+
         $session = container(SessionManager::class);
         $userId = $session->get(SessionManager::USER_ID);
-        $bookings = BookingDAO::getBookingsForUser($userId, $currentPage);
-        $totalBookings = BookingDAO::getTotalBookingsForUser($userId);
 
+        $bookings = $this->bookingService->getBookingsByUserIdPerPage($userId, $currentPage);
+        $totalBookings = $this->bookingService->getTotalNumberOfBookingByUserId($userId);
+
+        // TODO: Refactor pages to a helper class?
         $totalPages = ceil($totalBookings / configs('pagination.bookings_per_page'));
         $previousPage = $currentPage - 1 >= 1 ? $currentPage - 1 : null;
         $nextPage = $currentPage + 1 <= $totalPages ? $currentPage + 1 : null;
