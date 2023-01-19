2ddf53e2a7971c0861e0fc4afd495bf311140749
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-01-19 12:04:52 +0100

    changed constrains for user deletion

--- migrations/012-tables-constrains.sql
+++ migrations/012-tables-constrains.sql
@@ -2,26 +2,26 @@ ALTER TABLE `desksharing_dev`.`bookings`
     ADD CONSTRAINT fk_booking_user
     FOREIGN KEY (user_id)
         REFERENCES `desksharing_dev`.`users`(id)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION;
+        ON DELETE CASCADE
+        ON UPDATE CASCADE;
 
 ALTER TABLE `desksharing_dev`.`bookings`
     ADD CONSTRAINT fk_booking_desk
     FOREIGN KEY (desk_id)
         REFERENCES `desksharing_dev`.`desks`(id)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION;
+        ON DELETE CASCADE
+        ON UPDATE CASCADE;
 
 ALTER TABLE `desksharing_dev`.`users_roles`
     ADD CONSTRAINT fk_users_roles_user
         FOREIGN KEY (user_id)
             REFERENCES `desksharing_dev`.`users`(id)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION;
+            ON DELETE CASCADE
+            ON UPDATE CASCADE;
 
 ALTER TABLE `desksharing_dev`.`users_roles`
     ADD CONSTRAINT fk_users_roles_role
         FOREIGN KEY (role_id)
             REFERENCES `desksharing_dev`.`roles`(id)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION;
+            ON DELETE CASCADE
+            ON UPDATE CASCADE;
